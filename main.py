d09e915cf3b1843b572144506edd58bc090155f5
joaquinauc <124112927+joaquinauc@users.noreply.github.com>
2024-10-29 23:48:46 -0700

    Update main.py
    Adding emails and searching for direct flights.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the
 # program requirements.
-from data_manager import DataManager
+from data_manager import DataManager, get_customer_emails
 from flight_search import FlightSearch
 from datetime import datetime, timedelta
 from flight_data import find_cheapest_flight
@@ -24,10 +24,26 @@ today = datetime.now()
 date_tomorrow = (today + timedelta(days=1)).strftime("%Y-%m-%d")
 date_after_six_months = (today + timedelta(days=182)).strftime("%Y-%m-%d")
 
+customer_data = get_customer_emails()
+customer_emails = [email["whatIsYourEmail?"] for email in customer_data]
+
 for data in sheet_data:
     flights = flight_search.get_offers(data["iataCode"], date_tomorrow, date_after_six_months)
-    cheapest_flight = find_cheapest_flight(flights)
+
+    try:
+        if "data" not in flights:
+            raise KeyError("No data found in the response.")
+        cheapest_flight = find_cheapest_flight(flights)
+    except KeyError:
+        flights = flight_search.get_offers(data["iataCode"], date_tomorrow, date_after_six_months, False)
+        if "data" in flights:
+            cheapest_flight = find_cheapest_flight(flights)
+    else:
+        print(f"No flight data found for {data["iataCode"]}, searching for the next destination...")
+        continue
+
     print(f"Lowest price from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}: ${cheapest_flight.price}")
-    notif_manager = NotificationManager(cheapest_flight)
+    notif_manager = NotificationManager(cheapest_flight, customer_emails)
     notif_manager.send_notification(data["lowestPrice"])
+    notif_manager.send_emails(data["lowestPrice"])
     time.sleep(2)
